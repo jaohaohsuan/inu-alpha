6607b751acaf3190b802dee1f82515814cda19d2
Henry Jao <jaohaohsuan@gmail.com>
2016-06-15 09:08:52 +0800

    sbt build fix

--- jenkins.sh
+++ jenkins.sh
@@ -6,13 +6,13 @@ source /root/.bashrc
 
 # prepare deployment
 # substitute ${version} in target/deployment/*.yaml and *.sh files
-sbt 'root' 'clean' 'compile' 'release'
+sbt 'project root' 'clean' 'compile' 'release'
 
 # make storedq-compute images
 sbt 'project cluster' 'clean' 'compile' 'test' 'docker:publish'
 
 chmod +x target/deployment/up.sh
-. target/deployment/up.sh
+./target/deployment/up.sh
 
 docker rmi $(docker images -f "dangling=true" -q) 2>&1 || true
 docker rmi $(docker images | grep 127.0.0.1:5000/inu/storedq-compute | tail -n +3 | awk '{print $3'}) 2>&1 || true

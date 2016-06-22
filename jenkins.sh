d46c2dd6dfd65d32c578b30cfb2540d975ffef85
Henry Jao <jaohaohsuan@gmail.com>
2016-06-22 18:00:05 +0800

    stop docker rmi

--- jenkins.sh
+++ jenkins.sh
@@ -17,9 +17,9 @@ sbt 'project frontend' 'clean' 'compile' 'test' 'docker:publish'
 chmod +x target/deployment/up.sh
 ./target/deployment/up.sh
 
-docker rmi $(docker images "dangling=true" -q) 2>&1 || true
-docker rmi $(docker images | grep 127.0.0.1:5000/inu/storedq-compute | tail -n +3 | awk '{print $3'}) 2>&1 || true
-docker rmi $(docker images | grep 127.0.0.1:5000/inu/storedq-api | tail -n +3 | awk '{print $3'}) 2>&1 || true
+# docker rmi $(docker images "dangling=true" -q) 2>&1 || true
+# docker rmi $(docker images | grep 127.0.0.1:5000/inu/storedq-compute | tail -n +3 | awk '{print $3'}) 2>&1 || true
+# docker rmi $(docker images | grep 127.0.0.1:5000/inu/storedq-api | tail -n +3 | awk '{print $3'}) 2>&1 || true
 
 set +x
 

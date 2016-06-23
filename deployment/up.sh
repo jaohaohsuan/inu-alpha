bf288d2b355ac6204902076f1e30f442d3a90687
Henry Jao <jaohaohsuan@gmail.com>
2016-06-23 10:50:59 +0800

    delay frontend deployment & set cassandra-query-journal.refresh-interval = 1s

--- deployment/up.sh
+++ deployment/up.sh
@@ -4,6 +4,10 @@ set -x
 
 kubectl apply -f target/deployment/manifests/svc.yaml
 
-kubectl apply -f target/deployment/manifests/deploy.yaml --record
+kubectl apply -f target/deployment/manifests/cluster-deploy.yaml --record
+
+sleep 20
+
+kubectl apply -f target/deployment/manifests/cluster-frontend.yaml --record
 
 set +x

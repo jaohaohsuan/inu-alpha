df56722fa4ccb752308ff911eb497ea735818580
Henry Jao <jaohaohsuan@gmail.com>
2016-09-15 00:39:30 +0800

    change to seed petset

--- deployment/up.sh
+++ deployment/up.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-kubectl apply -f target/deployment/manifests/svc.yaml
+#kubectl apply -f target/deployment/manifests/svc.yaml
 
-kubectl apply -f target/deployment/manifests/cluster-deploy.yaml --record
+kubectl apply -f target/deployment/manifests/seed-petset.yaml --record
 
 sleep 20
 
-kubectl apply -f target/deployment/manifests/frontend-deploy.yaml --record
+#kubectl apply -f target/deployment/manifests/frontend-deploy.yaml --record
 
 set +x

38ce9d2e683526c1de5f98e90017fbc7fee0ef10
Henry Jao <jaohaohsuan@gmail.com>
2017-05-25 16:04:02 +0800

    exit if es is not ready

--- frontend/src/main/scala/com/inu/frontend/Main.scala
+++ frontend/src/main/scala/com/inu/frontend/Main.scala
@@ -2,6 +2,7 @@ package com.inu.frontend
 
 import java.net.InetAddress
 
+import akka.actor.Status.Success
 import akka.actor.{ActorSystem, DeadLetter, Props}
 import akka.cluster.Cluster
 import akka.cluster.singleton.{ClusterSingletonProxy, ClusterSingletonProxySettings}
@@ -16,6 +17,7 @@ import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.util.{ Try, Success, Failure }
 
 
 object Main extends App with LazyLogging {
@@ -25,7 +27,8 @@ object Main extends App with LazyLogging {
   implicit val system = ActorSystem(config.getString("storedq.cluster-name"), config)
   implicit val executionContext = system.dispatcher
 
-  private lazy val client = {
+  private lazy val esClient = Try {
+
     val esAddr = InetAddress.getByName(config.getString("elasticsearch.transport-address"))
     val settings = org.elasticsearch.common.settings.Settings.settingsBuilder()
       .put("cluster.name", config.getString("elasticsearch.cluster-name"))
@@ -38,6 +41,7 @@ object Main extends App with LazyLogging {
   system.actorOf(Props[SeedNodeListener])
 
   val cluster = Cluster(system)
+
   cluster.registerOnMemberUp {
 
     system.actorOf(ClusterSingletonProxy.props(
@@ -49,25 +53,35 @@ object Main extends App with LazyLogging {
 
     val host = Config.host
     val port = Config.port
-    IO(Http).ask(Http.Bind(system.actorOf(Props(classOf[ServiceActor], client), "service"), interface = host, port = port))
-      .mapTo[Http.Event]
-      .map {
-        case Http.Bound(address) =>
-          println(s"storedq service version: ${com.inu.frontend.storedq.BuildInfo.version} bound to $address")
-        case Http.CommandFailed(cmd) =>
-          println("storedq service could not bind to " +  s"$host:$port, ${cmd.failureMessage}")
-          sys.exit(1)
-      }
 
-    IO(Http).ask(Http.Bind(system.actorOf(Props(classOf[DigServiceActor], client), "digService"), interface = host, port = 7880))
-      .mapTo[Http.Event]
-      .map {
-        case Http.Bound(address) =>
-          println(s"dig service version: ${com.inu.frontend.storedq.BuildInfo.version} bound to $address")
-        case Http.CommandFailed(cmd) =>
-          println("dig service could not bind to " +  s"$host:7880, ${cmd.failureMessage}")
-          sys.exit(1)
-      }
+    // try reach elasticsearch
+    esClient match {
+      case scala.util.Success(transportClient) =>
+        IO(Http).ask(Http.Bind(system.actorOf(Props(classOf[ServiceActor], transportClient), "service"), interface = host, port = port))
+          .mapTo[Http.Event]
+          .map {
+            case Http.Bound(address) =>
+              println(s"storedq service version: ${com.inu.frontend.storedq.BuildInfo.version} bound to $address")
+            case Http.CommandFailed(cmd) =>
+              println("storedq service could not bind to " +  s"$host:$port, ${cmd.failureMessage}")
+              sys.exit(1)
+          }
+
+        IO(Http).ask(Http.Bind(system.actorOf(Props(classOf[DigServiceActor], transportClient), "digService"), interface = host, port = 7880))
+          .mapTo[Http.Event]
+          .map {
+            case Http.Bound(address) =>
+              println(s"dig service version: ${com.inu.frontend.storedq.BuildInfo.version} bound to $address")
+            case Http.CommandFailed(cmd) =>
+              println("dig service could not bind to " +  s"$host:7880, ${cmd.failureMessage}")
+              sys.exit(1)
+          }
+
+      case Failure(ex) =>
+        system.log.error(ex,"elasticsearch service cannot be resolved")
+        sys.exit(1)
+    }
+
   }
 
   sys.addShutdownHook {
